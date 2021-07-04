c8f5f09688749f156559d40c947711ab4ce3065b
ssight <ssight@protonmail.com>
2021-07-04 20:23:56 +0100

    Add db-sentry-removal patch

---
+++ patches/dynamic/db-sentry-removal.ts
@@ -0,0 +1,16 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { getSrcFiles } from '../util';
+
+export default function removeDBException(rootDir: string) {
+    let srcFiles = getSrcFiles(rootDir);
+
+    srcFiles.forEach(srcFile => {
+        let src = readFileSync(srcFile, 'utf-8');
+        
+        let patchSrc = src.replace(/ADD_EXCEPTION_CONTEXT\((\r\n)?.*?;/g, "");
+
+        if (patchSrc.includes("ADD_EXCEPTION_CONTEXT"))  console.log("NO!");
+
+        writeFileSync(srcFile, patchSrc);
+    })
+}
