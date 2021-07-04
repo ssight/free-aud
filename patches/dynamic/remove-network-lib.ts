65df86666cb7f1f4f7b6c4a11a9f612f30b6bcb5
ssight <ssight@protonmail.com>
2021-07-04 19:50:20 +0100

    Add remove-network-lib code

---
+++ patches/dynamic/remove-network-lib.ts
@@ -0,0 +1,17 @@
+import { rmSync, readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+export default function patchRemoveNetworkLib(rootDir: string) {
+    let libDir = resolve(rootDir, "libraries"), libCMakePath = resolve(libDir, "CMakeLists.txt");
+
+    // rmSync(resolve(libDir, "lib-network-manager"), { recursive: true });
+    // rmSync(resolve(libDir, "lib-sentry-reporting"), { recursive: true });
+
+    let libCMake: string = readFileSync(libCMakePath, 'utf-8');
+    let patchCMake = libCMake
+        .replace(`if ( \${_OPT}has_networking )`, "").replace(`list( APPEND LIBRARIES lib-network-manager)`, "").replace(`endif()`, "")
+        .replace(`list( APPEND LIBRARIES lib-sentry-reporting)`, "");
+
+
+    writeFileSync(libCMakePath, patchCMake);
+}
