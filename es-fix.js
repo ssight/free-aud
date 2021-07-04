59ba091b5862ce08a3d7d2140d65182d1dd8619f
ssight <ssight@protonmail.com>
2021-07-04 20:13:15 +0100

    Add es-fix.js file to fix node.js `exports` errors

---
+++ es-fix.js
@@ -0,0 +1,16 @@
+// ES-FIX: A hacky but working solution to fix `exports` not being defined in node
+
+const fs = require('fs');
+const path = require('path');
+
+let dynamicPatches = fs.readdirSync("patches/dynamic");
+
+dynamicPatches.forEach(i => {
+    let patch = fs.readFileSync(path.resolve("patches/dynamic", i)).toString();
+    patch = patch.replace("exports.default = ", "module.exports = ");
+    fs.writeFileSync(path.resolve("patches/dynamic", i), patch);
+})
+
+let util = fs.readFileSync("patches/util.js").toString();
+util = util.replace("exports.", "module.exports.");
+fs.writeFileSync("patches/util.js", util);
