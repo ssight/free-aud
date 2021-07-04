8896137b8fbd0df95ab9d823dd4b2efd863e3147
ssight <ssight@protonmail.com>
2021-07-04 20:56:58 +0100

    Update remove-network-lib patch

--- patches/dynamic/remove-network-lib.ts
+++ patches/dynamic/remove-network-lib.ts
@@ -2,7 +2,7 @@ import { rmSync, readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 
 export default function patchRemoveNetworkLib(rootDir: string) {
-    let libDir = resolve(rootDir, "libraries"), libCMakePath = resolve(libDir, "CMakeLists.txt");
+    let libDir = resolve(rootDir, "libraries"), libCMakePath = resolve(libDir, "CMakeLists.txt"), srcCMakePath = resolve(rootDir, "src", "CMakeLists.txt");
 
     rmSync(resolve(libDir, "lib-network-manager"), { recursive: true });
     rmSync(resolve(libDir, "lib-sentry-reporting"), { recursive: true });
@@ -12,6 +12,11 @@ export default function patchRemoveNetworkLib(rootDir: string) {
         .replace(`if ( \${_OPT}has_networking )`, "").replace(`list( APPEND LIBRARIES lib-network-manager)`, "").replace(`endif()`, "")
         .replace(`list( APPEND LIBRARIES lib-sentry-reporting)`, "");
 
+    let srcCMake: string = readFileSync(srcCMakePath, 'utf-8');
+    let srcCMakePatch = srcCMake
+        .replace(/\$<\$<BOOL:\${\${_OPT}has_sentry_reporting}>:([\s\S]*?)>/gms, "")
+        .replace(/\$<\$<BOOL:\${\${_OPT}has_updates_check}>:([\s\S]*?)>/gms, "");
 
     writeFileSync(libCMakePath, patchCMake);
+    writeFileSync(srcCMakePath, srcCMakePatch);
 }
